d95508d5747988ab9f7956e9403f83603b212274
SlivkinArtem <113514162+SlivkinArtem@users.noreply.github.com>
2022-12-10 11:53:53 +0300

    lab5 matrix n*n

--- main.c
+++ main.c
@@ -1,54 +1,50 @@
 #include <stdio.h>
-#include "stdlib.h"
-#include "math.h"
-
-
-struct dot{
-    double x;
-    double y;
-};
-struct rectangle{
-    struct dot A;
-    struct dot B;
-    struct dot C;
-    struct dot D;
-};
-double lenght(const struct dot *a, const struct dot *b){
-    double len = sqrt(pow(a->x - b->x, 2)+pow(a->y - b->y, 2));
-    return len;
-};
-double area(const struct rectangle *r){
-    double a = lenght(&r->A, &r->B);
-    double b = lenght(&r->A, &r->C);
-    return a*b;
+#include <stdlib.h>
+void add(double **a, int n){
+for (int i = 0; i < n; i++)
+    for (int j = 0; j < n; j++){
+        scanf("%lg", &a[i][j]);
+    }
 }
-
-union {
-    int keyboard;
-    struct {
-        int elements: 1;
-        int numlock: 1;
-        int capslock: 1;
-        int scrolllock: 1;
-    } bit_Field;
-} lock;
-
-
-void main() {
-    enum months {
-        june, july, august
-    };
-    enum months chosen = july;
-    printf("%d\n", chosen);
-
-    struct dot A = {0,0};
-    struct dot B = {6, 0};
-    struct dot C = {0, 3};
-    struct dot D = {6, 3};
-
-    struct rectangle r = {A,B,C,D};
-    printf("%lf", area(&r));
-    scanf("%X", &lock.keyboard);
-    printf("%d %d %d %d", abs(lock.bit_Field.elements), abs(lock.bit_Field.numlock),
-           abs(lock.bit_Field.capslock), abs(lock.bit_Field.scrolllock));
+void mnzh(double **a, double **b, double **res, int n){
+double ans;
+for (int i = 0; i < n; i++){
+    for (int j = 0; j < n; j++){
+        ans = 0;
+        for (int k = 0; k < n; k++)
+            ans+=a[i][k] * b[k][j];
+            res[i][j] = ans;
+        }
+    }
+}
+void print(double **a, int n){
+for (int i = 0; i < n; i++){
+    for (int j = 0; j < n; j++){
+        printf("%lg %lg", a[i][j], a[i][j+1]);
+        printf("\n");
+        j++;
+        }
+    }
+}
+int main() {
+int n;
+double **a, **b, **res;
+scanf("%d", &n);
+a = (double**)malloc(sizeof(double*)*n);
+for (int i = 0; i < n; i++){
+    a[i] = (double *)malloc(n*sizeof(double));
+}
+b = (double**)malloc(sizeof(double*)*n);
+for (int i = 0; i < n; i++){
+    b[i] = (double*)malloc(n * sizeof(double));
+}
+res = (double**)malloc(sizeof(double*)* n);
+for (int i = 0; i < n; i++) {
+    res[i] = (double *) malloc(n * sizeof(double));
+}
+add(a, n);
+add(b, n);
+mnzh(a, b, res, n);
+print(res, n);
+return 0;
 }

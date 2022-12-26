f8d81f0a3e05c3d529dfc186ff04d9d8da9b8f26
SlivkinArtem <113514162+SlivkinArtem@users.noreply.github.com>
2022-12-26 15:17:50 +0300

    Update lab11.c

--- lab11/lab11.c
+++ lab11/lab11.c
@@ -4,9 +4,14 @@
 
 int main()
 {
-    struct dot a, b, c, d;
-    scanf("%lf %lf %lf %lf %lf %lf %lf", &a.x, &a.y, &b.x, &b.y, &c.x, &c.y, &d.x, &d.y);
-    struct square t = new_square(a,b,c,d);
-    printf("Perimeter of such square is: %f, Square: %f", per(t), area(t));
+    double x1, y1, x2, y2, x3, y3, x4, y4;
+    scanf("%lf %lf %lf %lf %lf %lf %lf %lf", &x1, &y1, &x2, &y2, &x3, &y3, &x4, &y4);
+    struct dot* a = new_dot(x1, y1);
+    struct dot* b = new_dot(x2, y2);
+    struct dot* c = new_dot(x3, y3);
+    struct dot* d = new_dot(x4, y4);
+    printf("%lf %lf %lf %lf %lf %lf %lf %lf", a->x, a->y, b->x, b->y, c->x, c->y, d->x, d->y);
+    struct square* t = new_square(a, b, c, d);
+    printf("\nPerimeter of such square is: %f, Square: %f", perimeter(t), area(t));
     return 0;
 }

83c9b2f55fd8b0a4a643566e1e63b1fb1215d635
SlivkinArtem <113514162+SlivkinArtem@users.noreply.github.com>
2022-12-26 15:18:26 +0300

    Update square.h

--- lab11/square.h
+++ lab11/square.h
@@ -6,22 +6,21 @@ struct dot {
 };
 
 struct square {
-    struct dot A;
-    struct dot B;
-    struct dot C;
-    struct dot D;
+    struct dot *A;
+    struct dot *B;
+    struct dot *C;
+    struct dot *D;
 };
 
 struct side {
     double x;
     double y;
-
 };
 
-struct dot new_dot(double, double);
-struct side new_side(struct dot, struct dot);
-struct square new_square(struct dot, struct dot, struct dot, struct dot);
-double length(struct side);
-double area(struct square);
-double per(struct square);
+struct dot* new_dot(double, double);
+struct side* new_side(struct dot*, struct dot*);
+struct square* new_square(struct dot*, struct dot*, struct dot*, struct dot*);
+double length(struct side*);
+double area(struct square*);
+double perimeter(struct square*);
 #endif //PROGLAB11_SQUARE_H
